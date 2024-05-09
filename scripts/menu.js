406b0b80392c7de5934bc03f8c8fbae0304d5d2f
jthoeun <johnny.thoeun@gmail.com>
2024-05-08 20:08:21 -0600

    minor changes

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,5 @@
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('nav');
+const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
