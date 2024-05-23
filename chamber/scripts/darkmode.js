fcccea462d3431afb37b17bfcfc7898793a27d7c
jthoeun <johnny.thoeun@gmail.com>
2024-05-22 20:41:30 -0600

    added dark mode to chamber

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const header = document.querySelector("header")
+const footer = document.querySelector("footer");
+const nav = document.querySelector("nav, ul,li");
+
+modeButton.addEventListener("click", () => {
+    body.classList.toggle('dark-mode');
+    main.classList.toggle('dark-mode');
+    header.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+    nav.toggle('dark-mode');
+});
