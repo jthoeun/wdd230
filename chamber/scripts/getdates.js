19e8f26e741a5746f1bbc089394f5d4729896a56
jthoeun <johnny.thoeun@gmail.com>
2024-06-15 09:37:58 -0600

    added form content to chamber site. fixes to root index to attempt to span map

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,3 +2,11 @@ document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
 let text = document.lastModified;
 document.getElementById("modified").innerHTML = text;
+const currentTimeInput = document.getElementById("currentTime");
+currentTimeInput.value = Date.now();
+
+const myForm = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+    console.log("Form submitted with current time:", currentTimeInput.value);
+});
