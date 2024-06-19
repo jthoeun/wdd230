25c1262651a6722a02bc4e2157648b25bed8b1b2
jthoeun <johnny.thoeun@gmail.com>
2024-06-18 20:00:15 -0600

    added code to prophets.js file

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,11 +1,11 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+    displayProphets(data.prophets);
+
 }
 
 getProphetData();
@@ -17,16 +17,16 @@ const displayProphets = (prophets) => {
         let portrait = document.createElement('img');
 
         // Build the h2 content out to show the prophet's full name
-        fullName.textContent = `${prophet.name, lastname}  ____________`; // fill in the blank
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name, lastname} ______________`); // fill in the blank
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
-        card.appendChild(cards); //fill in the blank
+        card.appendChild(fullName); //fill in the blank
         card.appendChild(portrait);
 
         cards.appendChild(card);
