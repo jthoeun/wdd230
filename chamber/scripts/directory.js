6a1f8146789b1c5da1e9c1773297ec4335a794c2
jthoeun <johnny.thoeun@gmail.com>
2024-06-22 17:53:23 -0600

    discover page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,5 @@
 const url = "https://jthoeun.github.io/wdd230/chamber/data/members.json"
-const cards = document.querySelector('#cards');
+const card = document.querySelector('#cards');
 
 async function getCompanyData() {
     const response = await fetch(url);
@@ -13,15 +13,19 @@ const displayCompany = (companies) => {
     companies.forEach((companies) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
-        let companyName = document.createElement('h2'); // fill in the blank
+        let companyName = document.createElement('h3'); // fill in the blank
         let logo = document.createElement('img');
+        let address = document.createElement('p')
         let website = document.createElement('p')
 
         // Build the h2 content out to show the prophet's full name
         companyName.textContent = `${companies.name}`;
 
+        //Address
+        address.textContent = `${companies.address}`;
+
         // Build the image portrait by setting all the relevant attributes
-        logo.setAttribute('src', logo);
+        logo.setAttribute('src', companies.imageurl);
         logo.setAttribute('alt', `logo of ${companies.name}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '340');
@@ -32,6 +36,8 @@ const displayCompany = (companies) => {
         // Append the section(card) with the created elements
         card.appendChild(companyName); //fill in the blank
         card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(website);
 
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
