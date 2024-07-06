8cce0f1779ca375656bfb8e9aa5a3511ef435df8
jthoeun <johnny.thoeun@gmail.com>
2024-07-06 16:17:28 -0700

    minor changes to spotlight.js

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,12 +1,12 @@
 function showGoldMembers() {
-    const companiesData = fetch('https://jthoeun.github.io/wdd230/chamber/data/members.json') // Replace with the path to your JSON file
+    const companiesData = fetch('https://jthoeun.github.io/wdd230/chamber/data/members.json')
         .then(response => response.json())
         .then(data => data.companies);
 
     companiesData.then(companies => {
         const goldMembers = companies.filter(company => company.membershipLevel === "Gold");
 
-        // Update your HTML with the filtered companies
+
         const goldMemberList = document.getElementById("gold-members");
         goldMemberList.innerHTML = ""; // Clear existing content
 
